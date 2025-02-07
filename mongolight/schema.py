e66c0f9f38383378c694a838b7d59c706472293b
Bryan Jesus Ramon Avila <bryanjesus.ra@gmail.com>
2025-02-07 15:20:00 -0500

    feat: basic code

--- mongolight/schema.py
+++ mongolight/schema.py
@@ -9,13 +9,14 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
         Create a collection for the given model.
         """
         collection = model._meta.db_table
-        if collection not in self.connection.list_collection_names():
-            self.connection.create_collection(collection)
+        if collection not in self.connection.connection.list_collection_names():
+            self.connection.connection.create_collection(collection)
 
         # Crear índices
         for field in model._meta.local_fields:
             if field.db_index:
-                self.connection[collection].create_index(field.column)
+                self.connection.connection[collection].create_index(
+                    field.column)
 
     def delete_model(self, model):
         """
@@ -28,4 +29,4 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     def add_field(self, model, field):
         if field.db_index:
             collection = model._meta.db_table
-            self.connection[collection].create_index(field.column)
+            self.connection.connection[collection].create_index(field.column)
