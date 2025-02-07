a16b29654aae40a5cdccc82914e766c94f5e70ae
Bryan Jesus Ramon Avila <bryanjesus.ra@gmail.com>
2025-02-07 16:36:18 -0500

    feat: basic code

--- mongolight/introspection.py
+++ mongolight/introspection.py
@@ -2,9 +2,18 @@ from django.db.backends.base.introspection import BaseDatabaseIntrospection
 
 
 class DatabaseIntrospection(BaseDatabaseIntrospection):
+    """
+    Introspection class for MongoDB.
+    """
+
     def get_table_list(self, cursor):
+        """
+        Return a list of table (collection) names in the database.
+        """
         return self.connection.connection.list_collection_names()
 
-    def get_table_description(self, cursor, table_name):
-        # Obtener la estructura de la colección (pseudo-esquema)
-        return []
+    def table_names(self, cursor=None, include_views=False):
+        """
+        Return a list of table (collection) names in the database.
+        """
+        return self.get_table_list(cursor)
